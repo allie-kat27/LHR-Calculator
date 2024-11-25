c7eace102184376788ab9adaeb37fdae002f9354
allie-kat27 <alliem740@gmail.com>
2024-11-25 16:26:02 -0500

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+"use client";
+import LaserPackageCalculator from '../components/LaserPackageCalculator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 md:p-8">
+      <LaserPackageCalculator />
+    </main>
+  );
+}
